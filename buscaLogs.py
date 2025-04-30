11b8a71a2bbb4f95e0383eb8bc729d9240eb36e8
BrunoSerpa <serpabruno2005@gmail.com>
2025-04-30 09:09:16 -0300

    Atualizei para o sistema puxar os arquivos .evtx do Windows na pasta C:\Windows\System32\winevt\Logs

--- buscaLogs.py
+++ buscaLogs.py
@@ -4,7 +4,7 @@ def verificaSistema():
     sistema = name
     if sistema == 'nt':
         print('O sistema é Windows')
-        return r'C:\Windows\Logs', sistema
+        return r'C:\Windows\System32\winevt\Logs', sistema
     elif sistema == 'posix':
         print('O sistema é Linux')
         return '/var/log/', sistema
@@ -12,7 +12,7 @@ def verificaSistema():
         print('Sistema não identificado')
         return None, None
 
-def verificaDiretorio(diretorio):
+def verificaDiretorio(diretorio, sistema):
     if not path.exists(diretorio):
         print(f'O diretório {diretorio} não existe')
         return []
@@ -21,12 +21,21 @@ def verificaDiretorio(diretorio):
         print(f'O caminho {diretorio} não é um diretório')
         return []
 
-    logs = [
-        path.join(root, file)
-        for root, _, files in walk(diretorio)
-        for file in files
-        if file.lower().endswith('.log')
-    ]
+    if sistema == 'nt':
+        logs = [
+            path.join(root, file)
+            for root, _, files in walk(diretorio)
+            for file in files
+            if file.lower().endswith('.evtx')
+        ]
+    else:
+        logs = [
+            path.join(root, file)
+            for root, _, files in walk(diretorio)
+            for file in files
+            if file.lower().endswith('.log')
+        ]
+
 
     if not logs:
         print(f'⚠️ Nenhum arquivo .log encontrado em {diretorio}')
@@ -37,9 +46,9 @@ def main():
     dir_logs, sistema = verificaSistema()
     if dir_logs is None:
         return [], None
-    return verificaDiretorio(dir_logs), sistema
+    return verificaDiretorio(dir_logs, sistema), sistema
 
 if __name__ == "__main__":
-    logs = main()
+    logs, _ = main()
     for i, log in enumerate(logs, start=1):
         print(f'{i}º log: {path.basename(log)}')
