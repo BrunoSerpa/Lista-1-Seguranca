5505ac5c86e659ab5479570aa0f04d98a3cb46c7
BrunoSerpa <serpabruno2005@gmail.com>
2025-05-07 09:50:51 -0300

    Atualizei para o sistema utilizar a função de transformarLogs e tentartNovamente e a pasta logs no Windows

--- buscaLogs.py
+++ buscaLogs.py
@@ -1,18 +1,27 @@
 from os import name, path, walk
+from pyuac import isUserAdmin, runAsAdmin
+
+from transformarLogs import main as transformaLogs
+from tentarNovamente import main as tentarNovamente
 
 def verificaSistema():
     sistema = name
     if sistema == 'nt':
-        print('O sistema é Windows')
-        return r'C:\Windows\System32\winevt\Logs', sistema
+        if isUserAdmin():
+            transformaLogs()
+        else:
+            print('O sistema é Windows')
+            if tentarNovamente('Deseja converter os logs atuais do Windows'):
+                runAsAdmin()
+        return path.abspath(path.join(path.dirname(__file__), "logs"))
     elif sistema == 'posix':
         print('O sistema é Linux')
-        return '/var/log/', sistema
+        return '/var/log/'
     else:
         print('Sistema não identificado')
-        return None, None
+        return None
 
-def verificaDiretorio(diretorio, sistema):
+def verificaDiretorio(diretorio):
     if not path.exists(diretorio):
         print(f'O diretório {diretorio} não existe')
         return []
@@ -21,21 +30,12 @@ def verificaDiretorio(diretorio, sistema):
         print(f'O caminho {diretorio} não é um diretório')
         return []
 
-    if sistema == 'nt':
-        logs = [
-            path.join(root, file)
-            for root, _, files in walk(diretorio)
-            for file in files
-            if file.lower().endswith('.evtx')
-        ]
-    else:
-        logs = [
-            path.join(root, file)
-            for root, _, files in walk(diretorio)
-            for file in files
-            if file.lower().endswith('.log')
-        ]
-
+    logs = [
+        path.join(root, file)
+        for root, _, files in walk(diretorio)
+        for file in files
+        if file.lower().endswith('.log')
+    ]
 
     if not logs:
         print(f'⚠️ Nenhum arquivo .log encontrado em {diretorio}')
@@ -43,12 +43,12 @@ def verificaDiretorio(diretorio, sistema):
     return logs
 
 def main():
-    dir_logs, sistema = verificaSistema()
+    dir_logs = verificaSistema()
     if dir_logs is None:
-        return [], None
-    return verificaDiretorio(dir_logs, sistema), sistema
+        return []
+    return verificaDiretorio(dir_logs)
 
 if __name__ == "__main__":
-    logs, _ = main()
+    logs = main()
     for i, log in enumerate(logs, start=1):
         print(f'{i}º log: {path.basename(log)}')
