09d0f3e48630f988eacbe015ec6ed04b3085fba0
BrunoSerpa <serpabruno2005@gmail.com>
2025-05-10 10:14:07 -0300

    Refatorei as funções

--- exercicio03.py
+++ exercicio03.py
@@ -1,50 +1,69 @@
 # Faça  um  programa  que  retorne  os  últimos  30  comandos  executados  por  um usuário no sistema.
-from conteudoLog import main as conteudoLog
 from os import name
+from datetime import datetime
 
-def extrair_comandos(descricoes, maquina):
-    if maquina == "posix":
-        return descricoes[0]
-    for descricao in descricoes:
-        if "CommandLine=" in descricao:
-            comando = descricao.split("=", 1)[1].strip()
-            if comando:
-                return comando
-        if "HostApplication=" in descricao:
-            comando = descricao.split("=", 1)[1].strip()
-            if comando and not comando.lower().endswith("powershell.exe"):
-                return comando
+from conteudoLog import main as conteudoLog
 
-    return None
+def extrairComandos(descricoes):
+    if name == "nt":
+        for descricao in descricoes:
+            if "CommandLine=" in descricao:
+                comando = descricao.split("=", 1)[1].strip()
+                if comando:
+                    return comando
 
-def main(maquina = None):
-    if not maquina:
-        maquina = name
+        for descricao in descricoes:
+            if "HostApplication=" in descricao:
+                comando = descricao.split("=", 1)[1].strip()
+                if comando:
+                    return comando
+    return descricoes[0]
+
+def geraTimestamp(registro):
+    try:
+        ((ano, mes, dia), hora, _, _, _) = registro
+        ts = datetime.strptime(f"{ano}-{mes}-{dia} {hora}", "%Y-%m-%d %H:%M:%S")
+        return ts
+    except Exception:
+        return datetime.min
+
+def main():
     logs = []
-    if maquina == "nt":
+    if name == "nt":
         from os import path
-        logs.append(path.join(path.dirname(__file__), 'logs', 'Windows PowerShell.log'))
-    elif maquina == "posix":
+        logs.append(path.join(path.dirname(__file__), 'logs', 'Windows PowerShell.log')) #logs/Windows PowerShell.log
+
+    elif name == "posix":
         from pathlib import Path
         logs.append(Path.home() / ".bash_history")
         logs.append(Path.home() / ".zsh_history")
 
     registros = []
     for log in logs:
-        registros = [*reversed(conteudoLog(log))]
+        registros.extend(conteudoLog(log))
+
     if not registros:
         print("Nenhum registro encontrado.")
         return
+
+    registros.sort(key=geraTimestamp, reverse=True)
     mensagens = []
-    for posicao, ((ano, mes, dia), hora, titulo, ip, descricao) in enumerate(registros, start=1):
-        textoComando = extrair_comandos(descricao, maquina)
-        if textoComando:
-            mensagens.append(f"{len(registros) - posicao + 1}º Registro - {titulo} | IP: {ip} (Data: {dia}/{mes}/{ano} | Hora: {hora}):")
-            mensagens.append(f'Comando: {textoComando}')
+    for posicao, ((ano, mes, dia), hora, titulo, ip, descricao) in enumerate(registros, start=0):
+        textoComando = extrairComandos(descricao)
+        if not textoComando:
+            continue
+
+        numeroRegistro = len(registros) - posicao
+        mensagens.append(f"{numeroRegistro}º Registro - {titulo} | IP: {ip} (Data: {dia}/{mes}/{ano} | Hora: {hora}):")
+        mensagens.append(f'Comando: {textoComando}')
+
         if len(mensagens) == 60:
             break
+
     if not mensagens:
         print("Nenhum comando encontrado.")
+
+    print("Comandos encontrados:")
     for mensagem in mensagens:
         print(mensagem)
 
