21e39d2b9e7610db90b0a2c167c76440d6f00324
BrunoSerpa <serpabruno2005@gmail.com>
2025-05-10 10:14:30 -0300

    Refatorei as funções

--- exercicio04.py
+++ exercicio04.py
@@ -1,22 +1,26 @@
 # Faça  um  programa  que  conte  quantos  acessos  um  determinado  IP  fez  em  um  sistema.
-from conteudoLog import main as conteudoLog
-from os import name, path
+from os import name
 from collections import defaultdict
 
-def extrai_sid_usuario(descricao):
-    """Extrai apenas o primeiro SID de usuário da descrição."""
-    for parte in descricao:
-        if parte.startswith("S-1-5-"):
-            return parte.split()[0]
-    return "desconhecido"
+from conteudoLog import main as conteudoLog
 
-def main(maquina=None):
-    if not maquina:
-        maquina = name
+def extraiTipoAcesso(descricoes):
+    if name == "nt":
+        for descricao in descricoes:
+            if descricao.startswith("S-1-5-"):
+                return descricao.split()[0]
+        return "desconhecido"
+    return descricoes[0]
 
+def main():
     logs = []
-    if maquina == "nt":
-        logs.append(path.join(path.dirname(__file__), 'logs', 'Security.log'))
+    if name == "nt":
+        from os import path
+        logs.append(path.join(path.dirname(__file__), 'logs', 'Security.log')) #logs/Security.log
+    elif name == "posix":
+        from pathlib import Path
+        logs.append(Path.home() / ".bash_history")  # Histórico do bash
+        logs.append(Path.home() / ".zsh_history")   # Histórico do zsh
 
     registros = []
     for log in logs:
@@ -26,15 +30,30 @@ def main(maquina=None):
         print("Nenhum registro encontrado.")
         return
 
-    contagem_usuarios = defaultdict(int)
-
+    contagem = defaultdict(int)
     for (_, _, _), _, _, _, descricao in registros:
-        sid = extrai_sid_usuario(descricao)
-        contagem_usuarios[sid] += 1
+        sid = extraiTipoAcesso(descricao)
+        contagem[sid] += 1
 
-    print("Contagem de ações por usuário (SID primário):\n")
-    for sid, contagem in contagem_usuarios.items():
-        print(f"Usuário (SID): {sid} — {contagem} {"ação" if contagem == 1 else "ações"}")
+    mensagems = []
+    if name == "nt":
+        for sid, contagem in contagem.items():
+            mensagems.append(f"Usuário (SID): {sid} — {contagem} aç{"ão" if contagem == 1 else "ões"}")
+        if not mensagems:
+            print("Nenhuma de ação de usuário encontrada.")
+            return
+        print("Contagem de ações por usuário (SID primário):")
+    else:
+        print("Contagem de acesso por IP:\n")
+        for ip, contagem in contagem.items():
+            mensagems.append(f"(IP): {ip} — {contagem} acesso{"" if contagem == 1 else "s"}")
+        if not mensagems:
+            print("Nenhum acesso por IP encontrado.")
+            return
+        print("Contagem de acesso por IP:")
+
+    for mensagem in mensagems:
+        print(mensagem)
 
 if __name__ == "__main__":
     main()
