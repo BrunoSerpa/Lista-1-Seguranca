2184606b11f0e65a58efa165d7292638d8fcfe37
BrunoSerpa <serpabruno2005@gmail.com>
2025-05-10 10:17:22 -0300

    Refatorei as funções

--- exercicio05.py
+++ exercicio05.py
@@ -1,30 +1,24 @@
 # Faça  um  programa  que  registre  eventos  de  auditoria,  como  quando  um  usuário acessa  ou  altera  um  recurso.  Cada  evento  deve  ser  salvo  em  um  arquivo  com informações como: data, hora, nome do usuário, ação realizada, etc.
-from conteudoLog import main as conteudoLog
 from os import name
-from os import path
 
-def extraiUsuario(descricao, maquina):
-    if maquina == 'posix':
-        return
-    for parte in descricao:
-        if "MicrosoftAccount:user=" in parte:
-            return parte.split("=", 1)[1].split()[0]
-    return "desconhecido"
+from conteudoLog import main as conteudoLog
 
-def main(maquina=None):
-    if not maquina:
-        maquina = name
-
+def extraiUsuario(descricoes):
+    if name == "nt":
+        for descricao in descricoes:
+            if "MicrosoftAccount:user=" in descricao:
+                return descricao.split("=", 1)[1].split()[0]
+        return "desconhecido"
+    return descricoes[0]
+def main():
     logs = []
-    if maquina == "nt":
-        logs.append(path.join(path.dirname(__file__), 'logs', 'Security.log'))
-    elif maquina == "posix":
-        # Linux (comum: auditoria via journald ou histórico de comandos)
-        # logs.append("/var/log/auth.log")  # Debian/Ubuntu
-        # logs.append("/var/log/audit/audit.log")  # RedHat/CentOS/Fedora com auditd
-        # logs.append(Path.home() / ".bash_history")  # Histórico do bash
-        # logs.append(Path.home() / ".zsh_history")   # Histórico do zsh
-        pass
+    if name == "nt":
+        from os import path        
+        logs.append(path.join(path.dirname(__file__), 'logs', 'Security.log')) #logs/Security.log
+    elif name == "posix":
+        from pathlib import Path
+        logs.append(Path.home() / ".bash_history")  # Histórico do bash
+        logs.append(Path.home() / ".zsh_history")   # Histórico do zsh
 
     registros = []
     for log in logs:
@@ -36,7 +30,7 @@ def main(maquina=None):
 
     mensagens = []
     for (ano, mes, dia), hora, titulo, ip, descricao in registros:
-        usuario = extraiUsuario(descricao, maquina)
+        usuario = extraiUsuario(descricao)
         inicio='Um' if usuario=='desconhecido' else 'O'
         mensagens.append(
             f'{inicio} usuário {usuario} realizou ações na auditoria de "{titulo}", IP: {ip} no dia {dia}/{mes}/{ano} às {hora}.'
